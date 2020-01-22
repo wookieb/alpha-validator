9ef0916203a6b102e03ded642f4eb5b13c85b55e
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2020-01-22 23:24:18 +0100

    Fixed module

--- packages/module/src/Module.ts
+++ packages/module/src/Module.ts
@@ -3,6 +3,7 @@ import {Container, Definition, onActivation} from "alpha-dic";
 import {References} from "./References";
 import {Validator} from "alpha-validator";
 import {Annotation, PREDICATE} from "./schemaValidationAnnotation";
+import {SchemaValidator} from "./SchemaValidator";
 
 export class Module extends _Module<{ container: Container }> {
 
@@ -22,11 +23,11 @@ export class Module extends _Module<{ container: Container }> {
         return Definition.create(References.VALIDATOR)
             .useConstructor(Validator)
             .annotate(onActivation(async function (this: Container, service: Validator) {
-                for (const [schemaValidation, _ann] of await this.getByAnnotation(PREDICATE, true)) {
+                for (const [schemaValidation, _ann] of await this.getByAnnotation<SchemaValidator>(PREDICATE, true)) {
                     const annotation = _ann as Annotation;
 
                     for (const schemaName of annotation.schemaNames) {
-                        service.registerValidationForSchema(schemaName, schemaValidation);
+                        service.registerValidationForSchema(schemaName, schemaValidation.validate.bind(schemaValidation));
                     }
                 }
                 return service;
